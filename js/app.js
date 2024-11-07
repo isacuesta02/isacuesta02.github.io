fe7a2a0eaefeed41b92a01aaaea413376b4b03de
isacuesta02 <isabellaandreacuestaniebles@gmail.com>
2024-11-06 19:17:19 -0500

    Firts commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// js/app.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Página cargada correctamente");
+    // Aquí puedes añadir cualquier funcionalidad personalizada.
+});
